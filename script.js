d085e458d660ba0b2fad99d2be1f76084f60d4e2
Naveen Nirban Yadav <naveen.nirban@soliterasoftware.com>
2021-07-03 14:03:52 +0530

    # Updated

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
 fetch('https://jsonplaceholder.typicode.com/users')
-.then(function (response) {
-    return response.json();
-})
-.then(function (data) {
-    appendData(data);
-
-   // console.log(data);
-});
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+        // console.log(data);
+    });
 
 function appendData(data) {
-let mainContainer = document.getElementById("list");
-for (let i = 0; i < data.length; i++) {
+    let mainContainer = document.getElementById("list");
+    for (let i = 0; i < data.length; i++) {
 
-    let div = document.createElement("li");
-    li.setAttribute("data-id",i+1);
-    li.setAttribute("id",i+1);
-    div.innerHTML = data[i].name + ',' + 'id:' + data[i].id + ', username:' + data[i].username + ', email:' + data[i].email + ', phone' + data[i].phone + ', website:' + data[i].website;
+        let div = document.createElement("li");
+        li.setAttribute("data-id", i + 1);
+        li.setAttribute("id", i + 1);
+        div.innerHTML = data[i].name + ',' + 'id:' + data[i].id + ', username:' + data[i].username + ', email:' + data[i].email + ', phone' + data[i].phone + ', website:' + data[i].website;
 
-    mainContainer.appendChild(div);
-let datasetvalue=li.getAttribute("data-id");
+        mainContainer.appendChild(div);
+        let datasetvalue = li.getAttribute("data-id");
+        
 
-;
-}
+    }
 };
-
 
 let li = document.getElementById("list");
 //let datasetvalue= li.getAttribute("data-id");
 //console.log( datasetvalue)
 li.onclick = function () {
-location.href = "index2.html?Id="+datasetvalue;
+    location.href = "index2.html?Id=" + datasetvalue;
 }
-
